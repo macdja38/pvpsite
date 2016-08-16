07a73bb7c4d65cd934de6c1670d8281c6abd4e45
macdja38 <jakeincanada@icloud.com>
2016-08-16 02:23:55 -0400

    Removed comments, added favicon

--- src/components/Html.js
+++ src/components/Html.js
@@ -10,7 +10,17 @@ function Html({ title, description, style, script, children }) {
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+
+
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#102372" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
+
         <style id="css" dangerouslySetInnerHTML={{ __html: style }} />
       </head>
       <body>
