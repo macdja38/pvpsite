03a625de4b5f7ba6b5031865b4bf8776aa9180bf
macdja38 <jakeincanada@icloud.com>
2016-09-15 19:19:44 -0400

    No longer use connection. more explicit responses.
    No longer use connection now that we are using rethinkdbdash for a database connector.
    
    Respond to requests with a more explicit error message.

--- src/api/v1/user.js
+++ src/api/v1/user.js
@@ -16,15 +16,19 @@ function checkServerAuth(req, res, next) {
 
 function checkUser(req, res, next) {
   if (req.isAuthenticated() && req.user.id) return next();
-  res.status(403).send("User not Authenticated");
+  res.status(403).send('User not Authenticated');
   return true;
 }
 
-module.exports = function register(app, { r, connPromise }) {
+module.exports = function register(app, { r }) {
   app.get('/api/v1/user/', checkUser, (req, res) => {
-    console.log("Got user request");
-    if (req.user) { res.json(req.user); console.log(req.user.username); }
-    else res.status(404).send('User not Cached');
+    console.log('Got user request');
+    if (req.user) {
+      res.json(req.user);
+      console.log(req.user.username);
+    } else {
+      res.status(404).send('User not Cached');
+    }
   });
 
   /*  "/api/v1/prefix/:id"
@@ -35,37 +39,36 @@ module.exports = function register(app, { r, connPromise }) {
   app.get('/api/v1/user/:id', checkServerAuth, (req, res) => {
     if (req.user) res.json(req.user);
     else {
-      connPromise.then((conn) => r
-        .table('users').get(req.params.id)
-        .then(user => {
-          res.json(user);
-        })
-      );
+      r
+      .table('users')
+      .get(req.params.id)
+      .run()
+      .then(user => {
+        res.json(user);
+      });
     }
   });
 
   app.put('/api/v1/user/:id', checkServerAuth, (req, res) => {
-    connPromise.then((conn) => {
-      const prefix = { prefix: req.body.prefix, id: req.params.id };
-      r.table('servers').insert(prefix, { conflict: 'update' }).run(conn)
-        .then(() => {
-          res.json({ success: true });
-        })
-        .catch(() => {
-          res.json({ success: false });
-        });
-    });
-  });
-
-  app.delete('/api/v1/user/:id', checkServerAuth, (req, res) => connPromise.then((conn) => {
     const prefix = { prefix: req.body.prefix, id: req.params.id };
-    r.table('servers').insert(prefix, { conflict: 'update' }).run(conn)
+    r.table('servers').insert(prefix, { conflict: 'update' }).run()
       .then(() => {
         res.json({ success: true });
       })
       .catch(() => {
         res.json({ success: false });
       });
-  }));
+  });
+
+  app.delete('/api/v1/user/:id', checkServerAuth, (req, res) => {
+    const prefix = { prefix: req.body.prefix, id: req.params.id };
+    r.table('servers').insert(prefix, { conflict: 'update' }).run()
+    .then(() => {
+      res.json({ success: true });
+    })
+    .catch(() => {
+      res.json({ success: false });
+    });
+  });
 };
 
