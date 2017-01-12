f5c0257c6b6e269188ac0a8c6f2e3222f0f3e038
macdja38 <jakeincanada@icloud.com>
2017-01-12 09:24:59 -0500

    add handler to take care of changes to the input fields

--- src/routes/customConfig/CustomConfig.js
+++ src/routes/customConfig/CustomConfig.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './CustomConfig.css';
 import ServerMenu from '../../components/ServerMenu';
@@ -11,110 +11,152 @@ function childOrObject(item, key) {
   return {};
 }
 
-function thingOrDefault(settings, defaults, key = 'value') {
+function thingOrDefault(delta, settings, defaults, key = 'value') {
+  if (delta.hasOwnProperty(key)) return delta[key];
   if (settings.hasOwnProperty(key)) return settings[key];
   if (defaults.hasOwnProperty(key)) return defaults[key];
   return true;
   throw new Error('no setting or default value defined');
 }
 
-function toDivs(settingsMap, settings, serverId, botId, urlLocation, currentPath) {
-  console.log('Iteration', urlLocation);
-  console.log('settings:', settings);
-  console.log('settingsMap:', settingsMap);
-  console.log('urlLocation:', urlLocation);
-  if (settingsMap.type === 'pageSelector') {
-    return (<div key={settingsMap.key}>
-      <CustomServerMenu
-        botId={botId}
-        serverId={serverId}
-        pages={Object.keys(settingsMap.children)}
-        page={urlLocation[0]}
-      />
-      {toDivs(settingsMap.children[urlLocation[0]],
-        childOrObject(settings, urlLocation[0]),
-        serverId,
-        botId,
-        urlLocation.slice(1),
-        currentPath.slice(0).push(settingsMap.key))}
-    </div>);
-  } else if (settingsMap.type === 'category') {
-    return (<InputBox key={settingsMap.key}>
-      <div>{
-      settingsMap.children.map((c) =>
-        toDivs(c,
-          childOrObject(settings, c.key),
-          serverId,
-          botId,
-          urlLocation,
-          currentPath.slice(0).push(settingsMap.key)))
-    }</div>
-    </InputBox>);
-  } else if (settingsMap.type === 'boolean') {
-    console.log(thingOrDefault(settings, settingsMap));
-    return (
-      <InputBox key={settingsMap.key}>
-        <div key={settingsMap.key}>
-          {settingsMap.name}
-          <input
-            type="checkbox"
-            name={settingsMap.name}
-            alt={settingsMap.description}
-            checked={thingOrDefault(settings, settingsMap)}
-          />
-        </div>
-      </InputBox>
-    );
-  } else if (settingsMap.type === 'int') {
-    return (
-      <InputBox key={settingsMap.key}>
-        <div key={settingsMap.key}>
-          {settingsMap.name}
-          <input
-            type="text"
-            name={settingsMap.name}
-            alt={settingsMap.description}
-            value={thingOrDefault(settings, settingsMap)}
-          />
-        </div>
-      </InputBox>
-    );
-  } else if (settingsMap.type === 'list') {
-    return (
-      <InputBox key={settingsMap.key}>
-        <div key={settingsMap.key}>
-          {settingsMap.name}
-          <input
-            type="text"
-            name={settingsMap.name}
-            alt={settingsMap.description}
-            value={thingOrDefault(settings, settingsMap)}
-          />
-        </div>
-      </InputBox>
-    );
-  }
-  return <div key={settingsMap.key}>{settingsMap.key}</div>;
+function duplicateAndPush(array, ...params) {
+  const newArray = array.slice(0);
+  newArray.push(...params);
+  return newArray;
 }
 
-function CustomConfig({ user, serverId, settings, botId, title, guild, settingsMap, urlLocation }) {
-  // const avatarURL = `https://discordapp.com/api/users/85257659694993408/avatars/${user.avatar}.jpg`;
-  if (!guild) {
-    return (<div><h2>Guild {serverId} Not Found</h2>Your Guild Could not be found</div>);
+class CustomConfig extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { settings: props.settings.data, delta: {} };
+
+    this.toDivs = this.toDivs.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  return (
-    <Layout user={user}>
-      <div>
-        <ServerMenu className={s.nav} user={user} serverId={serverId} page="server" />
-        <div className={s.root}>
-          <div className={s.container}>
-            <h1 className={s.title}>{title} changed</h1>
-            {toDivs(settingsMap.layout, settings.data, serverId, botId, urlLocation.split('/'))}
+
+  handleChange(key) {
+    return (event) => {
+      console.log(key);
+      console.log(event.target.value);
+      this.state[key] = event.target.value;
+    };
+  }
+
+  handleSubmit() {
+    console.log(this);
+  }
+
+  toDivs(settingsMap, settings, delta, serverId, botId, urlLocation, currentPath) {
+    console.log('Iteration', urlLocation);
+    console.log('settings:', settings);
+    console.log('settingsMap:', settingsMap);
+    console.log('urlLocation:', urlLocation);
+    if (settingsMap.type === 'pageSelector') {
+      return (<div key={settingsMap.key}>
+        <CustomServerMenu
+          botId={botId}
+          serverId={serverId}
+          pages={Object.keys(settingsMap.children)}
+          page={urlLocation[0]}
+        />
+        {this.toDivs(settingsMap.children[urlLocation[0]],
+          childOrObject(settings, urlLocation[0]),
+          childOrObject(delta, urlLocation[0]),
+          serverId,
+          botId,
+          urlLocation.slice(1),
+          duplicateAndPush(currentPath, settingsMap.key))}
+      </div>);
+    } else if (settingsMap.type === 'category') {
+      return (<InputBox key={settingsMap.key}>
+        <div>{
+          settingsMap.children.map((c) =>
+            this.toDivs(c,
+              childOrObject(settings, c.key),
+              childOrObject(delta, c.key),
+              serverId,
+              botId,
+              urlLocation,
+              duplicateAndPush(currentPath, settingsMap.key)))
+        }</div>
+      </InputBox>);
+    } else if (settingsMap.type === 'boolean') {
+      console.log(thingOrDefault(delta, settings, settingsMap));
+      return (
+        <InputBox key={settingsMap.key}>
+          <div key={settingsMap.key}>
+            {settingsMap.name}
+            <input
+              type="checkbox"
+              onChange={this.handleChange(duplicateAndPush(currentPath, settingsMap.key))}
+              name={settingsMap.name}
+              alt={settingsMap.description}
+              checked={thingOrDefault(delta, settings, settingsMap)}
+            />
+          </div>
+        </InputBox>
+      );
+    } else if (settingsMap.type === 'int') {
+      return (
+        <InputBox key={settingsMap.key}>
+          <div key={settingsMap.key}>
+            {settingsMap.name}
+            <input
+              type="text"
+              onChange={this.handleChange(duplicateAndPush(currentPath, settingsMap.key))}
+              name={settingsMap.name}
+              alt={settingsMap.description}
+              value={thingOrDefault(delta, settings, settingsMap)}
+            />
+          </div>
+        </InputBox>
+      );
+    } else if (settingsMap.type === 'list') {
+      return (
+        <InputBox key={settingsMap.key}>
+          <div key={settingsMap.key}>
+            {settingsMap.name}
+            <input
+              type="text"
+              onChange={this.handleChange(duplicateAndPush(currentPath, settingsMap.key))}
+              name={settingsMap.name}
+              alt={settingsMap.description}
+              value={thingOrDefault(delta, settings, settingsMap)}
+            />
+          </div>
+        </InputBox>
+      );
+    }
+    return <div key={settingsMap.key}>{settingsMap.key}</div>;
+  }
+
+  render() {
+    const { user, serverId, botId, title, guild, settingsMap, urlLocation } = this.props;
+    // const avatarURL = `https://discordapp.com/api/users/85257659694993408/avatars/${user.avatar}.jpg`;
+    if (!guild) {
+      return (<div><h2>Guild {serverId} Not Found</h2>Your Guild Could not be found</div>);
+    }
+    return (
+      <Layout user={user}>
+        <div>
+          <ServerMenu className={s.nav} user={user} serverId={serverId} page="server" />
+          <div className={s.root}>
+            <div className={s.container}>
+              <h1 className={s.title}>{title} changed</h1>
+              {this.toDivs(settingsMap.layout,
+                this.state.settings,
+                this.state.delta,
+                serverId,
+                botId,
+                urlLocation.split('/'),
+                [])}
+            </div>
           </div>
         </div>
-      </div>
-    </Layout>
-  );
+      </Layout>
+    );
+  }
 }
 
 CustomConfig.propTypes = {
