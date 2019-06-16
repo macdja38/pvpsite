785926e938e4106763e6316fe21110d43cbea97e
Jake <jakeincanada@icloud.com>
2019-06-16 14:15:07 -0400

    Update User Avatar URLs

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -14,20 +14,11 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Navigation.css';
 import Link from '../Link';
 
-const DefaultAvatarHashes = [
-  '6debd47ed13483642cf09e832ed0bc1b',
-  '322c936a8c8be1b803cd94861bdfa868',
-  'dd4dbc0016779df1378e7812eabaa04d',
-  '0e291f67c9274a1abdddeb3fd919cbaa',
-  '1cbd08c76f8af6dddce02c5138971129',
-];
-
 function getUserAvatar(user) {
   if (user.avatar) {
-    return `https://discordapp.com/api/users/${user.id}/avatars/${user.avatar}.jpg`;
+    return `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`;
   }
-  const defaultAvatarHash = DefaultAvatarHashes[user.discriminator % DefaultAvatarHashes.length];
-  return `https://discordapp.com/assets/${defaultAvatarHash}.png`;
+  return `https://cdn.discordapp.com/embed/avatars/${user.discriminator % 5}.png`;
 }
 
 function Navigation({ className, user }) {
